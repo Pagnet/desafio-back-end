9e85078cf887bca792208c93a4c95ebbb8b8d0b6
Washington Silva <w-osilva@hotmail.com>
2019-10-18 21:15:15 -0300

    actions in transactions controller

--- src/app/controllers/transactions_controller.rb
+++ src/app/controllers/transactions_controller.rb
@@ -2,11 +2,23 @@ class TransactionsController < ApplicationController
 
   # GET /transactions
   def index
-    @stores = Store.all
+    @pagy, @stores = pagy(Store.all, items: 10)
   end
 
   # POST /transactions/upload
   def upload
+    @feedback = ''
+    if params[:file]
+      begin
+        File.open(params[:file].path, 'r').each do |line|
+          ImportTransactionJob.perform_now(line)
+        end
+        @feedback = 'completed'
+      rescue => e
+        @feedback = 'failed'
+      end
+    end
+    render layout: false
   end
 
 end
