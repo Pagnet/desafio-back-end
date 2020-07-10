7d94a67a551e9522b45891e56a0b914c078096c9
Rafael Porto <faielils@gmail.com>
2020-07-10 19:31:51 -0400

    Setting timezone and locale.
    Signed-off-by: Saulo Santiago <saulodasilvasantiago@gmail.com>

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,8 @@ module DesafioRubyBackend
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.time_zone = 'Brasilia'
+    
+    config.i18n.default_locale = :'pt-BR'
   end
 end
