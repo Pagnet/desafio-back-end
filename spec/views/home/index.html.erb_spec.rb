e3ba6d8181ee44b5b628c268e59f90ef7842202f
Rafael Porto <faielils@gmail.com>
2020-07-11 00:46:59 -0400

    Adding tests for the home view.
    Signed-off-by: Saulo Santiago <saulodasilvasantiago@gmail.com>

--- spec/views/home/index.html.erb_spec.rb
+++ spec/views/home/index.html.erb_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
-RSpec.describe "home/index.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe 'home/index.html.erb', type: :view do
+  it 'displays the given text' do
+    render
+
+    expect(rendered).to have_content('Upload')
+  end
+
+  it 'displays given elements' do
+    render
+
+    expect(rendered).to have_selector('input[type="file"]')
+    expect(rendered).to have_selector('input[type="submit"]')
+  end
 end
