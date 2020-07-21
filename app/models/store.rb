d5b9a4f9e0ad66baf9b10f564ecc2aa90f943d5d
Lucio Charallo <hi@lucio.app>
2020-07-21 01:48:32 -0300

    added method balance to Store

--- app/models/store.rb
+++ app/models/store.rb
@@ -2,4 +2,11 @@ class Store < ApplicationRecord
   has_many :transactions, dependent: :restrict_with_exception
 
   validates :name, presence: true, uniqueness: true
+
+  def balance
+    total_by_kinds = transactions.joins(:transaction_type).group(:kind).sum(:amount)
+    inputs = total_by_kinds[TransactionType.kinds[:input]] || 0
+    outputs = total_by_kinds[TransactionType.kinds[:output]] || 0
+    inputs - outputs
+  end
 end
