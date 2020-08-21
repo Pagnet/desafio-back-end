97fa15fe85ea2d29e21da3a3745108c6722fadc8
Caio Nascimento <contato@caionascimento.com.br>
2020-08-20 22:55:21 -0300

    Improves CNAB file processing validation
    Signed-off-by: Saulo Santiago <saulodasilvasantiago@gmail.com>

--- app/layers/service/financial_entry/import.rb
+++ app/layers/service/financial_entry/import.rb
@@ -3,26 +3,35 @@ module Service
     class Import
       def self.execute(file:, **config)
         parser = config.fetch(:parser) { FinancialEntryParserCNAB.new }
+
         associated_stores = Hash.new do |hash, (store_name, store_owner)|
           hash[[store_name, store_owner]] = ::Store.find_or_create_by!(name: store_name, owner: store_owner)
         end
 
         ActiveRecord::Base.transaction do
-          File.foreach(file).reduce(0) do |count, line|
-            entry_data = parser.parse(line).transform_values!(&:squish)
-            store = associated_stores[entry_data.values_at(:store_name, :store_owner)]
+          processed_count = 0
 
-            store.financial_entries.create!(
-              kind: ::FinancialEntry.kinds.key(entry_data[:kind].to_i),
-              transaction_date: entry_data[:transaction_date],
-              amount: entry_data[:amount],
-              social_number: entry_data[:social_number],
-              card_number: entry_data[:card_number],
-              transaction_time: entry_data[:transaction_time],
-            )
+          File.foreach(file) do |line|
+            if (line = line.squish).present?
+              entry_data = parser.parse(line).transform_values!(&:squish)
+              store = associated_stores[entry_data.values_at(:store_name, :store_owner)]
 
-            count += 1
+              entry = store.financial_entries.create!(
+                kind: ::FinancialEntry.kinds.key(entry_data[:kind].to_i),
+                transaction_date: entry_data[:transaction_date],
+                amount: entry_data[:amount],
+                social_number: entry_data[:social_number],
+                card_number: entry_data[:card_number],
+                transaction_time: entry_data[:transaction_time],
+              )
+
+              processed_count += 1
+            end
           end
+
+          return processed_count
+        rescue StandardError => e
+          raise "Save failed at line #{processed_count + 1}: #{e.message}"
         end
       end
     end
--- spec/layers/service/financial_entry/import_spec.rb
+++ spec/layers/service/financial_entry/import_spec.rb
@@ -2,6 +2,16 @@ require 'rails_helper'
 
 RSpec.describe Service::FinancialEntry::Import do
   describe '.execute' do
+    context 'empty processing' do
+      subject do
+        described_class.execute(file: File.new(Rails.root.join('spec/fixtures/CNAB-blank.txt')))
+      end
+
+      it 'imports one financial entry' do
+        expect{ subject }.to_not change{ FinancialEntry.count }
+      end
+    end
+
     context 'single processing' do
       subject do
         described_class.execute(file: File.new(Rails.root.join('spec/fixtures/CNAB-single.txt')))
@@ -31,6 +41,26 @@ RSpec.describe Service::FinancialEntry::Import do
       it 'imports new financial entries' do
         expect{ subject }.to change{ FinancialEntry.count }.by(21)
           .and change{ Store.count }.by(5)
+      end
+    end
+
+    context 'failed decimal parse' do
+      subject do
+        described_class.execute(file: File.new(Rails.root.join('spec/fixtures/CNAB-error-amount.txt')))
+      end
+
+      it 'imports new financial entries' do
+        expect{ subject }.to raise_error(RuntimeError, "Save failed at line 1: invalid value for BigDecimal(): \"0000014A00\"")
+      end
+    end
+
+    context 'failed kind id' do
+      subject do
+        described_class.execute(file: File.new(Rails.root.join('spec/fixtures/CNAB-error-kind.txt')))
+      end
+
+      it 'imports new financial entries' do
+        expect{ subject }.to raise_error(RuntimeError, "Save failed at line 3: Validation failed: Kind can't be blank")
       end
     end
   end
