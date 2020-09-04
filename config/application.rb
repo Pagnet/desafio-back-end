8ce4b627019bc5bb7945b1353ed573b637f0059a
bzumpano <zumpano.lp@gmail.com>
2020-09-04 00:12:59 -0300

    define project default configs

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,19 @@ module DesafioBackEnd
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+
+    config.generators do |g|
+      g.test_framework :rspec
+      g.template_engine :haml
+
+      g.assets = false
+      g.helper = false
+      g.view_specs false
+    end
+
+    config.encoding = 'utf-8'
+    config.i18n.default_locale = 'pt-BR'
+    config.time_zone = 'Brasilia'
   end
 end
