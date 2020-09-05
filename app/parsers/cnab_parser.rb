ca09dc15f66a23439fa8743fca3ea35516c1aa89
bzumpano <zumpano.lp@gmail.com>
2020-09-05 02:55:33 -0300

    improve CNAB parser

--- app/parsers/cnab_parser.rb
+++ app/parsers/cnab_parser.rb
@@ -9,15 +9,57 @@ class CnabParser
   def call
     file.readlines.map do |row|
       {
-        type: row[0].strip,
-        date: row[1..8].strip,
-        value: row[9..18].strip,
-        cpf: row[19..29].strip,
-        card_number: row[30..41].strip,
-        time: row[42..47].strip,
-        owner: row[48..61].strip,
-        store: row[62..80].strip
+        operation_type: operation_type(row),
+        occurred_at: occurred_at(row),
+        value: value(row),
+        cpf: cpf(row),
+        card_number: card_number(row),
+        owner: owner(row),
+        store: store(row)
       }
     end
+  end
+
+  def operation_type(row)
+    Operation.operation_types.key(row[0].to_i).to_sym
+  end
+
+  def occurred_at(row)
+    Time.parse("#{row[1,4]}-#{row[5,2]}-#{row[7,2]} #{row[42,2]}:#{row[44,2]}:#{row[46,2]} UTC")
+  end
+
+  def value(row)
+    "#{operation_signal(row)}#{row[9,8]}.#{row[17, 2]}".to_f
+  end
+
+  def cpf(row)
+    row[19..29]
+  end
+
+  def card_number(row)
+    row[30..41]
+  end
+
+  def owner(row)
+    row[48..61].strip
+  end
+
+  def store(row)
+    row[62..80].strip
+  end
+
+  def operation_signal(row)
+    operation_input?(operation_type(row)) ? '+' : '-'
+  end
+
+  def operation_input?(operation_type)
+    %i[
+      debit
+      credit
+      loan
+      sales
+      ted
+      doc
+    ].include? operation_type
   end
 end
--- spec/parsers/cnab_parser_spec.rb
+++ spec/parsers/cnab_parser_spec.rb
@@ -9,26 +9,41 @@ describe CnabParser do
     tempfile.path
   end
 
-  let(:file_row) { '3201903010000014200096206760174753****3153153453JOÃO MACEDO   BAR DO JOÃO       '}
-
   let(:result) { parser.call }
 
   describe 'call' do
-    let(:expected) do
-      [
-        {
-          type: '3',
-          date: '20190301',
-          value: '0000014200',
-          cpf: '09620676017',
-          card_number: '4753****3153',
-          time: '153453',
-          owner: 'JOÃO MACEDO',
-          store: 'BAR DO JOÃO'
-        }
-      ]
-    end
 
-    it { expect(result).to eq(expected) }
+    {
+      debit: '+',
+      bank_slip: '-',
+      financing: '-',
+      credit: '+',
+      loan: '+',
+      sales: '+',
+      ted: '+',
+      doc: '+',
+      rent: '-'
+    }.each do |operation_type, operation_signal|
+      context "when operation_type :#{operation_type}" do
+        let(:operation_type_value) { Operation.operation_types[operation_type] }
+        let(:file_row) { "#{operation_type_value}201903010000014200096206760174753****3153153453JOÃO MACEDO   BAR DO JOÃO       " }
+
+        let(:expected) do
+          [
+            {
+              operation_type: operation_type,
+              occurred_at: Time.parse('2019-03-01 15:34:53 UTC'),
+              value: "#{operation_signal}142.00".to_f,
+              cpf: '09620676017',
+              card_number: '4753****3153',
+              owner: 'JOÃO MACEDO',
+              store: 'BAR DO JOÃO'
+            }
+          ]
+        end
+
+        it { expect(result).to eq(expected) }
+      end
+    end
   end
 end
