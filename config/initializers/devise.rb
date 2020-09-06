5b81d2f541f8ef1c291b15d795faa1242fe003f9
bzumpano <zumpano.lp@gmail.com>
2020-09-06 02:21:30 -0300

    fix oauth params

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -309,5 +309,5 @@ Devise.setup do |config|
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
 
-  config.omniauth :google_oauth2, 'GOOGLE_CLIENT_ID', 'GOOGLE_CLIENT_SECRET', {}
+  config.omniauth :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], {}
 end
