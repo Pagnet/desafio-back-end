d48905756ae789ff696e1bb8d72d40d5a3680531
Luis Novoa <luis.matte.novoa@gmail.com>
2020-10-04 19:12:48 -0400

    Adds oauth authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,9 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :omniauthable
+
+  def self.from_google(email:, password:)
+    create_with(password: password).find_or_create_by!(email: email)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   resources :transactions, only: %i[new index create]
 
   root 'transactions#index'
