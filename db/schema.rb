f8b301026b19c323c3fd400161fa7e806369d839
Leandro <leandrofrancarj@gmail.com>
2021-03-07 01:38:44 -0300

    feat(model/store): add

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_03_07_043200) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "stores", force: :cascade do |t|
+    t.string "name"
+    t.string "owner"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
---
+++ spec/factories/stores.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :store do
+    name { "Acme" }
+    owner { "John Doe" }
+  end
+end
