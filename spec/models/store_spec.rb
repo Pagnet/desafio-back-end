1ab081411af87bb04ae8fa5f2ba18d3990e2857b
Leandro <leandrofrancarj@gmail.com>
2021-03-07 02:00:12 -0300

    test(models/store): add model store

--- spec/models/store_spec.rb
+++ spec/models/store_spec.rb
@@ -7,6 +7,6 @@ describe Store, type: :model do
     it { is_expected.to be_valid }
     it { is_expected.to validate_presence_of(:name) }
     it { is_expected.to validate_presence_of(:owner) }
-    it {is_expected.to validate_uniqueness_of(:name) }
+    it { is_expected.to validate_uniqueness_of(:name) }
   end
 end
