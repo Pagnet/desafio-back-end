e6768a4adc092c444212984bdbc306b2ee373102
Leandro <leandrofrancarj@gmail.com>
2021-03-06 23:43:13 -0300

    feat(file_upload): add file upload form

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,31 @@
+# frozen_string_literal: true
+
 class HomeController < ApplicationController
   def index
-    
+  end
+
+  def upload
+    uploaded_file = params[:cnab_file] if params[:cnab_file].present?
+
+    unless uploaded_file.present?
+      flash[:error] = "You need to choose a file"
+      redirect_to(root_path)
+      return false
+    end
+
+    if File.zero?(uploaded_file)
+      flash[:error] = "File is empty"
+      redirect_to(root_path)
+      return false
+    end
+
+    unless uploaded_file.content_type == "text/plain"
+      flash[:error] = "Invalid file type"
+      redirect_to(root_path)
+      return false
+    end
+
+    flash[:success] = "File uploaded successfully"
+    redirect_to(root_path)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   root to: 'home#index'
+  post 'upload', controller: 'home', action: 'upload'
 end
