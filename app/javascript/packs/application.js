114168dbe539f73115e42b9f6cdd85b7b096b81e
Leandro <leandrofrancarj@gmail.com>
2021-03-08 01:26:53 -0300

    chore(bootstrap): add bootstrap initial config

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,9 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import 'bootstrap/dist/js/bootstrap'
+import 'bootstrap/dist/css/bootstrap'
+require("stylesheets/application.scss")
 
 Rails.start()
 Turbolinks.start()
