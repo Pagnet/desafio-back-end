3fc4f65467e3edc3d77becb33274f6244a7af714
Leandro <leandrofrancarj@gmail.com>
2021-03-08 01:16:51 -0300

    test(factories/transactions): add transaction factory attributes

--- spec/factories/transactions.rb
+++ spec/factories/transactions.rb
@@ -1,9 +1,12 @@
+# frozen_string_literal: true
 FactoryBot.define do
   factory :transaction do
     kind { 1 }
-    paid_at { "" }
-    amount_in_cents { "" }
+    paid_at { DateTime.now }
+    amount_in_cents { 1293 }
     document_number { "12345678910" }
     card_number { "MyString" }
+
+    store
   end
 end
