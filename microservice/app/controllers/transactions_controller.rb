25062d7c781dc89ad610552492d75bb37b89f56c
Naiguel <naiguel@MacBook-Pro-de-Naiguel.local>
2021-04-11 17:45:16 -0300

    refactor: remove output

--- microservice/app/controllers/transactions_controller.rb
+++ microservice/app/controllers/transactions_controller.rb
@@ -1,6 +1,5 @@
 class TransactionsController < ApplicationController
   def upload
-    puts transaction_params
     UseCase::Transaction::Upload.new(repo).call(transaction_params)
     render json: { message: "File uploaded successfully" }
   end
