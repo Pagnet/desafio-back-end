fa8a5d2daf39a01b3fc79c2fe0f3e102dfe747f9
Naiguel <naiguel@MacBook-Pro-de-Naiguel.local>
2021-04-11 17:15:47 -0300

    chore: use case on controller

--- microservice/app/controllers/transactions_controller.rb
+++ microservice/app/controllers/transactions_controller.rb
@@ -1,5 +1,17 @@
 class TransactionsController < ApplicationController
   def upload
-    # render json: { message: "Success" }
+    puts transaction_params
+    UseCase::Transaction::Upload.new(repo).call(transaction_params)
+    render json: { message: "File uploaded successfully" }
+  end
+
+  private
+
+  def transaction_params
+    params.require(:transaction).permit(:file)
+  end
+
+  def repo
+    @repo ||= TransactionRepository.new
   end
 end
