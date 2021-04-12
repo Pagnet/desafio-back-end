30f4a74c29ab6daabc84484ff11c4885b9368008
Naiguel <naiguel@MacBook-Pro-de-Naiguel.local>
2021-04-11 23:51:56 -0300

    feat: schema updated column types

--- microservice/db/schema.rb
+++ microservice/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_12_011350) do
+ActiveRecord::Schema.define(version: 2021_04_12_024413) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,8 +26,8 @@ ActiveRecord::Schema.define(version: 2021_04_12_011350) do
   create_table "transaction_types", force: :cascade do |t|
     t.integer "numeric_type"
     t.text "description"
-    t.string "operation_type"
-    t.string "signal"
+    t.integer "operation_type"
+    t.integer "signal"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
