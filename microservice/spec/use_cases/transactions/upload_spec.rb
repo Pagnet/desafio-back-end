64c61d0deeeba5434fb4530c545bd3cd36c8736e
Naiguel <naiguel@MacBook-Pro-de-Naiguel.local>
2021-04-12 05:17:47 -0300

    test: use case transaction upload

---
+++ microservice/spec/use_cases/transactions/upload_spec.rb
@@ -0,0 +1,38 @@
+require "rails_helper"
+
+RSpec.describe "UseCases Transactions Upload" do
+  describe "execute" do
+    it "should parse file" do
+      transactionType = TransactionType.create(
+        numeric_type: 3,
+        description: "Financiamento",
+        operation_type: "saída",
+        signal: "negative",
+      )
+
+      store = Store.create(
+        name: "BAR DO JOÃO",
+        owner: "JOÃO MACEDO",
+      )
+
+      expectedResult = [{
+        transaction_type_id: transactionType.id,
+        occurrence_date: "20190301",
+        amount: 142.0,
+        cpf: "09620676017",
+        card_number: "4753****3153",
+        occurrence_time: "153453",
+        store_id: store.id,
+      }]
+
+      file = Rack::Test::UploadedFile.new(
+        "#{Rails.root}/spec/support/attachments/CNAB-one-line.txt",
+        "application/txt"
+      )
+
+      result = UseCases::Transactions::Upload::CNAB::parse_file(file)
+
+      expect(result).to eq(expectedResult)
+    end
+  end
+end
