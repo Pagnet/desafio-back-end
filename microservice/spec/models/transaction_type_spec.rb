c6f811be2bcdc1d28f696e5db3d25741990b57c7
Naiguel <naiguel@MacBook-Pro-de-Naiguel.local>
2021-04-11 22:29:20 -0300

    test: transaction type model tests

--- microservice/spec/models/transaction_type_spec.rb
+++ microservice/spec/models/transaction_type_spec.rb
@@ -1,5 +1,42 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe TransactionType, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject {
+    described_class.new(numeric_type: 1,
+                        description: "Débito",
+                        operation_type: "Entrada",
+                        signal: "Positive")
+  }
+
+  it "is valid with correct attributes" do
+    expect(subject).to be_valid
+  end
+
+  it "is not valid without numeric_type" do
+    subject.numeric_type = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid without description" do
+    subject.description = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid without operation_type" do
+    subject.operation_type = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid without signal" do
+    subject.signal = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid with invalid operation_type" do
+    expect { subject.operation_type = "Teste" }.to raise_error(ArgumentError)
+  end
+
+  it "is not valid with invalid signal" do
+    expect { subject.signal = "teste" }.to raise_error(ArgumentError)
+  end
 end
