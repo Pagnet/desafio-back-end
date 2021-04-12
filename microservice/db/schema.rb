32f6ec90b0b96db07ebbd3bcfc8745d5073104f2
Naiguel <naiguel@MacBook-Pro-de-Naiguel.local>
2021-04-11 22:31:08 -0300

    feat: schema structure updated

---
+++ microservice/db/schema.rb
@@ -0,0 +1,51 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2021_04_12_011350) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "stores", force: :cascade do |t|
+    t.string "owner"
+    t.string "name"
+    t.decimal "amount_total", default: "0.0"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "transaction_types", force: :cascade do |t|
+    t.integer "numeric_type"
+    t.text "description"
+    t.string "operation_type"
+    t.string "signal"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "transactions", force: :cascade do |t|
+    t.date "occurrence_date"
+    t.decimal "amount"
+    t.string "cpf", limit: 11
+    t.string "card_number", limit: 12
+    t.string "occurrence_time", limit: 6
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "transaction_type_id", null: false
+    t.bigint "store_id", null: false
+    t.index ["store_id"], name: "index_transactions_on_store_id"
+    t.index ["transaction_type_id"], name: "index_transactions_on_transaction_type_id"
+  end
+
+  add_foreign_key "transactions", "stores"
+  add_foreign_key "transactions", "transaction_types"
+end
