3ff3056a5cc30d7e2c8d67811b5d084fb224a113
Naiguel <naiguel@MacBook-Pro-de-Naiguel.local>
2021-04-12 05:16:38 -0300

    feat: create store repository

---
+++ microservice/lib/repositories/store_repository.rb
@@ -0,0 +1,10 @@
+module Repositories
+  class StoreRepository
+    def self.find_or_create(attributes)
+      Store.find_or_create_by(
+        :name => attributes[:name].strip,
+        :owner => attributes[:owner].strip,
+      )
+    end
+  end
+end
