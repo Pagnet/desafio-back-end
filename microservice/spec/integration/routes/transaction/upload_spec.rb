928d648a9c3de73522b0852289c5613aade74546
Naiguel <naiguel@MacBook-Pro-de-Naiguel.local>
2021-04-12 02:12:06 -0300

    test: request testing

--- microservice/spec/integration/routes/transaction/upload_spec.rb
+++ microservice/spec/integration/routes/transaction/upload_spec.rb
@@ -7,10 +7,17 @@ RSpec.describe "Transactions routes", :type => :routing do
   end
 end
 
-RSpec.describe "Transaction file upload", :type => :request do
-  it "should create transactions from cnab file" do
-    post "/transactions/upload", :params => { :file => "cnab.txt" }
+RSpec.describe "Transaction file upload request", :type => :request do
+  it "should execute controller upload method" do
+    file = Rack::Test::UploadedFile.new(
+      "#{Rails.root}/spec/support/attachments/CNAB.txt",
+      "application/txt"
+    )
 
-    expect(response.body).to include("File uploaded successfully")
+    @expected = { message: "File uploaded successfully", status: 200 }
+
+    post "/transactions/upload", params: { :file => file }
+
+    expect(response.body).to eq(@expected.to_json)
   end
 end
