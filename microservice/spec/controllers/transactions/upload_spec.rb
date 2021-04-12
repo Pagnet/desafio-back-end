554f244df3d1e05d3b2b55e27b96eff5dc17d91f
Naiguel <naiguel@MacBook-Pro-de-Naiguel.local>
2021-04-12 03:15:07 -0300

    test: spy use case on controller test

--- microservice/spec/controllers/transactions/upload_spec.rb
+++ microservice/spec/controllers/transactions/upload_spec.rb
@@ -2,8 +2,8 @@ require "rails_helper"
 
 RSpec.describe TransactionsController, type: :controller do
   describe "upload" do
-    it "call import_cnab method" do
-      allow(Transaction).to receive(:import_cnab)
+    it "call use case to upload cnab" do
+      allow(UseCases::Transactions::Upload::CNAB).to receive(:execute)
 
       file = Rack::Test::UploadedFile.new(
         "#{Rails.root}/spec/support/attachments/CNAB.txt",
@@ -12,7 +12,7 @@ RSpec.describe TransactionsController, type: :controller do
 
       post "upload", params: { file: file }
 
-      expect(Transaction).to have_received(:import_cnab)
+      expect(UseCases::Transactions::Upload::CNAB).to have_received(:execute)
 
       expect(response.status).to eq(200)
     end
