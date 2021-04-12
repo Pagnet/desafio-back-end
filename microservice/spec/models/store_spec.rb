f42c52ffe09e2e97b2e6dff5bfe67252db9d7a07
Naiguel <naiguel@MacBook-Pro-de-Naiguel.local>
2021-04-12 03:23:03 -0300

    test: store model

--- microservice/spec/models/store_spec.rb
+++ microservice/spec/models/store_spec.rb
@@ -1,5 +1,28 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Store, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject {
+    described_class.new(
+      owner: "JOÃO MACEDO  ",
+      name: "BAR DO JOÃO       ",
+    )
+  }
+
+  it "should create new with correct attributes" do
+    expect(subject).to be_valid
+  end
+
+  it "should create amount_total with default value zero" do
+    expect(subject.amount_total).to eq(0.0)
+  end
+
+  it "should not create when owner not exist" do
+    subject.owner = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "should not create when name not exist" do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
 end
