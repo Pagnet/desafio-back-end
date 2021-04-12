7974473e5d401334a98288453c94c7b5f875ac7b
Naiguel <naiguel@MacBook-Pro-de-Naiguel.local>
2021-04-11 22:28:23 -0300

    feat: validations to transaction type model

--- microservice/app/models/transaction_type.rb
+++ microservice/app/models/transaction_type.rb
@@ -1,2 +1,12 @@
 class TransactionType < ApplicationRecord
+  validates_presence_of :numeric_type
+  validates_presence_of :description
+  validates_presence_of :operation_type
+  validates_presence_of :signal
+
+  enum operation_type: ["Entrada", "Saída"]
+  validates :operation_type, inclusion: { in: ["Entrada", "Saída"] }
+
+  enum signal: ["Positive", "Negative"]
+  validates :signal, inclusion: { in: ["Positive", "Negative"] }
 end
