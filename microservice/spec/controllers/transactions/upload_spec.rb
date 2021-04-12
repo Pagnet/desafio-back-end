35da88476140e6b186a6f7723311a318ce4c9fe5
Naiguel <naiguel@MacBook-Pro-de-Naiguel.local>
2021-04-12 02:11:40 -0300

    test: transaction controller test

--- microservice/spec/controllers/transactions/upload_spec.rb
+++ microservice/spec/controllers/transactions/upload_spec.rb
@@ -1,6 +1,20 @@
 require "rails_helper"
 
-RSpec.describe "Transactions controller", :type => :controller do
-  it "call the use case > transaction > upload" do
+RSpec.describe TransactionsController, type: :controller do
+  describe "upload" do
+    it "call import_cnab method" do
+      allow(Transaction).to receive(:import_cnab)
+
+      file = Rack::Test::UploadedFile.new(
+        "#{Rails.root}/spec/support/attachments/CNAB.txt",
+        "application/txt"
+      )
+
+      post "upload", params: { file: file }
+
+      expect(Transaction).to have_received(:import_cnab)
+
+      expect(response.status).to eq(200)
+    end
   end
 end
