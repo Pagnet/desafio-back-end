2c8a0b023dbd43c13e694ab13a9a640c9e8797dd
Naiguel <naiguel@MacBook-Pro-de-Naiguel.local>
2021-04-11 22:29:02 -0300

    test: transaction spec tests

--- microservice/spec/models/transaction_spec.rb
+++ microservice/spec/models/transaction_spec.rb
@@ -1,5 +1,81 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Transaction, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  transactionType = TransactionType.create(numeric_type: 1,
+                                           description: "Débito",
+                                           operation_type: "Entrada",
+                                           signal: "Positive")
+
+  store = Store.create(owner: "JOÃO MACEDO  ", name: "BAR DO JOÃO       ")
+
+  subject {
+    described_class.new(
+      occurrence_date: "20190301",
+      amount: 0000014200 / 100,
+      cpf: "11111111111",
+      card_number: "4753****3153",
+      occurrence_time: "153453",
+      store_id: store.id,
+      transaction_type_id: transactionType.id,
+    )
+  }
+
+  it "is valid with correct attributes" do
+    expect(subject).to be_valid
+  end
+
+  it "is not valid without store_id relation" do
+    subject.store_id = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid without transaction_type_id relation" do
+    subject.transaction_type_id = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is valid with formatted occurrence_date" do
+    subject.occurrence_date = "2019-03-01"
+    expect(subject).to be_valid
+  end
+
+  it "is not valid with incorrect amount" do
+    subject.amount = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid with incorrect occurrence_date" do
+    subject.occurrence_date = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid with incorrect cpf" do
+    subject.cpf = "fakeCPF"
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid with incorrect cpf size" do
+    subject.cpf = "12345"
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid with incorrect card_number" do
+    subject.card_number = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid with incorrect card_number size" do
+    subject.card_number = "123809"
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid with incorrect occurrence_time" do
+    subject.occurrence_time = "faketime"
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid with incorrect occurrence_time size" do
+    subject.occurrence_time = "12504400"
+    expect(subject).to_not be_valid
+  end
 end
