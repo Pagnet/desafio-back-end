8683275c42b5fa266e6cbaf6dac202b5261779b4
Naiguel <naiguel@MacBook-Pro-de-Naiguel.local>
2021-04-13 03:42:04 -0300

    feat: transaction repository method

--- microservice/lib/repositories/transaction_repository.rb
+++ microservice/lib/repositories/transaction_repository.rb
@@ -1,7 +1,18 @@
 module Repositories
   class TransactionRepository
-    def self.findAll
-      Transaction.all
+    def self.all_with_relations
+      Transaction
+        .joins(:transaction_type)
+        .joins(:store)
+        .select(
+          "transactions.*, 
+          stores.name as store_name,
+          stores.owner as store_owner,
+          stores.amount_total as store_amount_total,
+          transaction_types.*"
+        )
+        .group("stores.id, transactions.id, transaction_types.id")
+        .order("stores.name ASC")
     end
 
     def self.create(transactions)
