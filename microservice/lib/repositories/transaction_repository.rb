b4de6a09a8fdbb9b678eb40b7ca32a506497768d
Naiguel <naiguel@MacBook-Pro-de-Naiguel.local>
2021-04-12 22:42:31 -0300

    feat: find all transaction repository

--- microservice/lib/repositories/transaction_repository.rb
+++ microservice/lib/repositories/transaction_repository.rb
@@ -1,5 +1,9 @@
 module Repositories
   class TransactionRepository
+    def self.findAll
+      Transaction.all
+    end
+
     def self.create(transactions)
       Transaction.create(transactions)
     end
