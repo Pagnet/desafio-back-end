bcb46f8cee04ef0d57a659de8d6b065b100d8d2f
Naiguel <naiguel@MacBook-Pro-de-Naiguel.local>
2021-04-12 22:33:17 -0300

    test: red for get transactions

--- microservice/spec/integration/routes/transaction/list_spec.rb
+++ microservice/spec/integration/routes/transaction/list_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 RSpec.describe "Transactions route", :type => :routing do
   it "should execute the GET /transactions" do
-    expect(post("/transactions")).
+    expect(get("/transactions")).
       to route_to("transactions#list")
   end
 end
