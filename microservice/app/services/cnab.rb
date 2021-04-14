5a5cf3db0a1e661d6bfc1fbb9d4924968b1252d3
Naiguel <naiguel@MacBook-Pro-de-Naiguel.local>
2021-04-14 05:26:29 -0300

    refactor: services layer

---
+++ microservice/app/services/cnab.rb
@@ -0,0 +1,14 @@
+module Cnab
+  def self.fields
+    {
+      transaction_type: 0..0,
+      occurrence_date: 1..8,
+      amount: 9..18,
+      cpf: 19..29,
+      card_number: 30..41,
+      occurrence_time: 42..47,
+      store_owner: 48..61,
+      store_name: 62..80,
+    }
+  end
+end
